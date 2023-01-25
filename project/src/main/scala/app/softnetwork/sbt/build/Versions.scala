b769558664e692e97ba103ed8820ae02a66c0247
Stéphane Manciot <stephane.manciot@gmail.com>
2023-01-25 09:10:45 +0100

    upgrade persistence api version

--- project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
+++ project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
@@ -2,11 +2,11 @@ package app.softnetwork.sbt.build
 
 object Versions {
 
-  val genericPersistence = "0.2.6"
+  val genericPersistence = "0.2.6.1"
 
   val akkaPersistenceJdbc = "5.0.4"
 
-  val server = "0.2.7"
+  val server = "0.2.8"
 
   val scalatest = "3.1.1"
 }
